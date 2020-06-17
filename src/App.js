331594b80ebad424b593904f2a9f02810c7ddc7d
ahmetizgi <dev.ahmetizgi@gmail.com>
2020-06-17 11:51:08 +0300

    tryout 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Netlify-demo
+          Hello from the other side
         </a>
       </header>
     </div>
